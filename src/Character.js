12aec854a48984f9eadf8ad4e6bc0bc512debb31
Thanos Korakas <thanoskorakas@gmail.com>
2018-07-05 02:10:53 +0300

    removed name from all characters list

--- src/Character.js
+++ src/Character.js
@@ -4,16 +4,16 @@ import Overdrive from 'react-overdrive'
 const Character = (props) => {
   return (
     <div className="character">
-        <React.Fragment>
-          <div className="character--title">
-            <strong><a onClick={(e) => { e.preventDefault(); props.viewCharacter(props.id) }} href="#">{props.name}</a></strong>
-          </div>
-          <div>
+      <a onClick={(e) => { e.preventDefault(); props.viewCharacter(props.id) }} href="#">
+        {/* <div className="character--title">
+          <strong>{props.name}</strong>
+        </div> */}
+        <div>
           <Overdrive id={props.name} duration={500} animationDelay={2}>
             <img className="character--image" alt={props.name} src={props.image} />
           </Overdrive>
-          </div>
-        </React.Fragment>
+        </div>
+      </a>
     </div>
   )
 }
